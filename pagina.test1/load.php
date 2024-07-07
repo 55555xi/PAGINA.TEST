e4e84cccc4664f7758f295527f7258cbc469cda8
Misael <123212477+55555xi@users.noreply.github.com>
2024-07-07 19:56:42 -0400

    Add files via upload

---
+++ pagina.test1/load.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>AWS Technical Essentials v4.1</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <div class="container">
+
+      <div class="row">
+    		<div class="col-md-12">
+      <nav class="navbar navbar-default" role="navigation">
+
+<div class="navbar-header">
+  <a class="navbar-brand" href="/"><img height="25" src="img/AWS_logo_RGB.png" /></a>
+</div>
+
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+  <ul class="nav navbar-nav">
+    <li>
+      <a href="load.php">Load Test</a>
+    </li>
+    <li>
+      <a href="rds.php">RDS</a>
+    </li>
+  </ul>
+</div>
+
+</nav>
+      <div class="jumbotron">
+
+      <meta http-equiv="refresh" content="5,URL=/load.php" />Under High CPU Load! (auto refresh in 5 seconds)
+      <hr />
+
+      <br /><p>Current CPU Load: <b>100%</b></p>
+    </p>
+    <p>
+    </p>
+  </div>
+</div>
+</div>
+</div>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/scripts.js"></script>
+  </body>
+</html>
